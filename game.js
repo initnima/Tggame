a259cd000a960aa1c8b75a8fecf07ee46aca1bd8
initnima <110024859+initnima@users.noreply.github.com>
2024-06-05 22:31:13 +0330

    Update game.js

--- game.js
+++ game.js
@@ -34,10 +34,20 @@ class Player {
         this.communication = 0;
         this.water = 0;
         this.food = 0;
-        this.coins = 0;
-        this.level = 1;
+        this.coins = parseFloat(localStorage.getItem('coins')) || 0;
+        this.level = parseInt(localStorage.getItem('level')) || 1;
         this.earnRate = 0.1;
-        this.upgradeCost = 0.4;
+        this.upgradeCosts = {
+            energy: 0.4,
+            colony: 0.4,
+            communication: 0.4,
+            water: 0.4,
+            food: 0.4
+        };
+        this.lastLoginTime = parseInt(localStorage.getItem('lastLoginTime')) || Date.now();
+
+        // Calculate offline earnings
+        this.calculateOfflineEarnings();
 
         // Start earning coins
         setInterval(() => {
@@ -50,25 +60,33 @@ class Player {
         // }, 5000); // 5 seconds
     }
 
+    calculateOfflineEarnings() {
+        const now = Date.now();
+        const timeElapsed = (now - this.lastLoginTime) / 300000; // 5 minutes in ms
+        const offlineEarnings = timeElapsed * this.earnRate;
+        this.coins += offlineEarnings;
+        this.lastLoginTime = now;
+        localStorage.setItem('lastLoginTime', now);
+        updateUI();
+    }
+
     earnCoins() {
         this.coins += this.earnRate;
+        localStorage.setItem('coins', this.coins);
         updateUI();
     }
 
     upgrade(parameter) {
-        if (this[parameter] !== undefined && this.coins >= this.upgradeCost) {
+        const cost = this.upgradeCosts[parameter];
+        if (this[parameter] !== undefined && this.coins >= cost) {
             this[parameter]++;
-            this.coins -= this.upgradeCost;
-            this.levelUp();
+            this.coins -= cost;
+            this.upgradeCosts[parameter] *= 2;
+            localStorage.setItem('coins', this.coins);
+            localStorage.setItem('lastLoginTime', Date.now());
+            localStorage.setItem(parameter, this[parameter]);
             updateUI();
         }
-    }
-
-    levelUp() {
-        this.level++;
-        this.earnRate += 0.1;
-        this.upgradeCost *= 2;
-        this.changePlanetColor();
     }
 
     changePlanetColor() {
@@ -94,7 +112,7 @@ function updateUI() {
     document.getElementById('coins').innerText = player.coins.toFixed(1);
     document.getElementById('level').innerText = player.level;
     document.getElementById('earnRate').innerText = player.earnRate.toFixed(1);
-    document.getElementById('upgradeCost').innerText = player.upgradeCost.toFixed(1);
+    document.getElementById('upgradeCost').innerText = player.upgradeCosts.energy.toFixed(1); // Just an example
 }
 
 // Initial UI update
@@ -110,7 +128,8 @@ function animate() {
 animate();
 
 // Attach upgrade function to window for button access
-window.upgrade = function(parameter) {
+window.upgrade = function() {
+    const parameter = document.getElementById('upgradeSelect').value;
     player.upgrade(parameter);
     updateUI();
 }
