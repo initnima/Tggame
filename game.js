aeb34c768f440f4352eb9ce6b7a3ce34d349d854
initnima <110024859+initnima@users.noreply.github.com>
2024-06-05 22:41:31 +0330

    Update game.js

--- game.js
+++ game.js
@@ -26,23 +26,36 @@ textureLoader.load('https://initnima.github.io/Tggame/a-breathtaking-3d-renderin
 
 camera.position.z = 15;
 
+// Update UI function
+function updateUI() {
+    document.getElementById('energy').innerText = player.energy;
+    document.getElementById('colony').innerText = player.colony;
+    document.getElementById('communication').innerText = player.communication;
+    document.getElementById('water').innerText = player.water;
+    document.getElementById('food').innerText = player.food;
+    document.getElementById('coins').innerText = player.coins.toFixed(1);
+    document.getElementById('level').innerText = player.level;
+    document.getElementById('earnRate').innerText = player.earnRate.toFixed(1);
+    document.getElementById('upgradeCost').innerText = player.upgradeCosts.energy.toFixed(1); // Just an example
+}
+
 // Player class
 class Player {
     constructor() {
-        this.energy = 0;
-        this.colony = 0;
-        this.communication = 0;
-        this.water = 0;
-        this.food = 0;
+        this.energy = parseInt(localStorage.getItem('energy')) || 0;
+        this.colony = parseInt(localStorage.getItem('colony')) || 0;
+        this.communication = parseInt(localStorage.getItem('communication')) || 0;
+        this.water = parseInt(localStorage.getItem('water')) || 0;
+        this.food = parseInt(localStorage.getItem('food')) || 0;
         this.coins = parseFloat(localStorage.getItem('coins')) || 0;
         this.level = parseInt(localStorage.getItem('level')) || 1;
         this.earnRate = 0.1;
         this.upgradeCosts = {
-            energy: 0.4,
-            colony: 0.4,
-            communication: 0.4,
-            water: 0.4,
-            food: 0.4
+            energy: parseFloat(localStorage.getItem('upgradeCostEnergy')) || 0.4,
+            colony: parseFloat(localStorage.getItem('upgradeCostColony')) || 0.4,
+            communication: parseFloat(localStorage.getItem('upgradeCostCommunication')) || 0.4,
+            water: parseFloat(localStorage.getItem('upgradeCostWater')) || 0.4,
+            food: parseFloat(localStorage.getItem('upgradeCostFood')) || 0.4
         };
         this.lastLoginTime = parseInt(localStorage.getItem('lastLoginTime')) || Date.now();
 
@@ -52,12 +65,7 @@ class Player {
         // Start earning coins
         setInterval(() => {
             this.earnCoins();
-        }, 5000); // 5 minutes
-
-        // For testing, you can use a shorter interval like 5 seconds (5000 ms)
-        // setInterval(() => {
-        //     this.earnCoins();
-        // }, 5000); // 5 seconds
+        }, 1000); // 5 minutes
     }
 
     calculateOfflineEarnings() {
@@ -83,8 +91,8 @@ class Player {
             this.coins -= cost;
             this.upgradeCosts[parameter] *= 2;
             localStorage.setItem('coins', this.coins);
-            localStorage.setItem('lastLoginTime', Date.now());
             localStorage.setItem(parameter, this[parameter]);
+            localStorage.setItem('upgradeCost' + parameter.charAt(0).toUpperCase() + parameter.slice(1), this.upgradeCosts[parameter]);
             updateUI();
         }
     }
@@ -101,19 +109,6 @@ class Player {
 
 // Initialize player
 const player = new Player();
-
-// Update UI function
-function updateUI() {
-    document.getElementById('energy').innerText = player.energy;
-    document.getElementById('colony').innerText = player.colony;
-    document.getElementById('communication').innerText = player.communication;
-    document.getElementById('water').innerText = player.water;
-    document.getElementById('food').innerText = player.food;
-    document.getElementById('coins').innerText = player.coins.toFixed(1);
-    document.getElementById('level').innerText = player.level;
-    document.getElementById('earnRate').innerText = player.earnRate.toFixed(1);
-    document.getElementById('upgradeCost').innerText = player.upgradeCosts.energy.toFixed(1); // Just an example
-}
 
 // Initial UI update
 updateUI();
