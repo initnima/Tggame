74ab63402228b06a212a0337869508f05ebfc6ea
initnima <110024859+initnima@users.noreply.github.com>
2024-06-06 00:09:39 +0330

    Update game.js

--- game.js
+++ game.js
@@ -96,7 +96,7 @@ class Player {
         // Start earning coins
         setInterval(() => {
             this.earnCoins();
-        }, 1000); // 5 minutes
+        }, 1000); // 5 minutes in milliseconds
 
         this.updateUI();
     }
@@ -152,16 +152,16 @@ class Player {
         document.getElementById('coins').innerText = this.coins.toFixed(1);
         document.getElementById('level').innerText = this.level;
         document.getElementById('earnRate').innerText = this.earnRate.toFixed(1);
-        updateUpgradeCostText();
+        this.updateUpgradeCostText();
+    }
+
+    updateUpgradeCostText() {
+        const parameter = document.getElementById('upgradeSelect').value;
+        document.getElementById('upgradeCost').innerText = this.upgradeCosts[parameter].toFixed(1);
     }
 }
 
 const player = new Player();
-
-function updateUpgradeCostText() {
-    const parameter = document.getElementById('upgradeSelect').value;
-    document.getElementById('upgradeCost').innerText = player.upgradeCosts[parameter].toFixed(1);
-}
 
 // Animation loop
 function animate() {
